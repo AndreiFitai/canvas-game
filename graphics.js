c101f4b0e338572d60d8889bf0bf391b3fd32bda
Andrei Fitai <fitai.andrei@gmail.com>
2018-08-14 20:40:09 +0200

    added script file for graphics

---
+++ graphics.js
@@ -0,0 +1,8 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d')
+var canvasWidth = canvas.width;
+var canvasHeight = canvas.height;
+
+
+var explosionImg = new Image();
+explosionImg.src = "/images/explosion.png"
